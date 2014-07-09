7d208dd6829ba982af3380c31f51bb72bac21281
tom zhou <zs68j2ee@gmail.com>
2014-07-09 22:05:21 +0800

    added windows start batch script

---
+++ start_win.bat
@@ -0,0 +1,10 @@
+@echo off
+rem Copyright 2014 iwebpp@gmail.com. All Rights Reserved.
+
+echo Make friends with We Browser!
+
+SETLOCAL
+
+cmd /c ".\bin\win\node.exe .\bin\win\deskshell.js"
+
+ENDLOCAL
